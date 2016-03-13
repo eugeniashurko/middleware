de90466f808fb949dd1feddf8868411965c05262
Eugenia <yarutoua@gmail.com>
2016-03-14 00:27:54 +0100

    Modified: pop node, remove specified node, get list of nodes

--- middleware.erl
+++ middleware.erl
@@ -4,23 +4,18 @@
 		 init_local_agent/2,
 		 master_agent_loop/2,
 		 empty_computation_loop/1,
-		 print_list/1,
 		 add_node/1,
 		 local_agent_loop/3,
 		 connect_local_agents/2,
 		 stop_cluster/1,
 		 stop_nodes/1,
 		 pop_node/1,
-		 remove_node/2
-		 % stop_computing_unit/1,
-		 % inform_local_agents/1
+		 remove_node/2,
+		 inform_local_agents/2,
+		 get_names/1,
+		 get_node_names/1,
+		 get_node_pids/1
 		]).
-
-
-print_list([ H ]) -> io:format("~p ~n", [H]), ok;
-print_list([ H | T ]) -> 
-	io:format("~p ~n", [H]),
-	print_list(T).
 
 % At the beginning we have master agent 
 start() ->
@@ -37,7 +32,7 @@ start() ->
 	
 init_local_agent(Master, AgentsAlive) ->
 	ComputingUnit = init_comp_unit(self()),
-	Name = list_to_atom(lists:flatten(io_lib:format("comp_node_~p", [node_id(AgentsAlive)]))),
+	Name = list_to_atom(lists:flatten(io_lib:format("comp_node_~p", [next_node_id(AgentsAlive)]))),
 	register(Name, ComputingUnit),
 	io:format("with computing unit at ~p ~n", [ComputingUnit]),
 	local_agent_loop(ComputingUnit, Master, AgentsAlive).
@@ -49,7 +44,7 @@ master_agent_loop(ComputingUnit, AgentsAlive) ->
 	receive
 		add -> 
 			NewPid = spawn(middleware, init_local_agent, [self(), AgentsAlive]),
-			Name = list_to_atom(lists:flatten(io_lib:format("node_~p", [node_id(AgentsAlive)]))),
+			Name = list_to_atom(lists:flatten(io_lib:format("node_~p", [next_node_id(AgentsAlive)]))),
 			register(Name, NewPid),
 			io:format("New agent on ~p initialized ~n", [NewPid]),
 			io:format("Registered under name: ~p~n~n", [Name]),
@@ -57,25 +52,74 @@ master_agent_loop(ComputingUnit, AgentsAlive) ->
 			connect_local_agents(NewPid, AgentsAlive),
 			% now we add the new agent to the list of agents alive
 			master_agent_loop(ComputingUnit, [NewPid | AgentsAlive]);
+		pop -> 
+			% now we just remove last added node - 
+			% but later maybe we will remove the node
+			% by some criteria - least loaded etc
+			
+			% check if there are nodes
+			case AgentsAlive of
+				[] ->
+					io:format("No nodes to remove: cluster contains a single node!~n", []),
+					io:format("(To remove master node stop the cluster)~n",[]),
+					master_agent_loop(ComputingUnit, AgentsAlive);
+				_ ->
+					[Node] = lists:sublist(AgentsAlive, 1),
+					Node ! stop,
+					NewAgentsAlive = lists:delete(Node, AgentsAlive),
+					inform_local_agents(Node, NewAgentsAlive),
+					{_, Name} = process_info(Node, registered_name),
+					unregister(Name),
+					io:format("Node '~p' (~p) removed~n", [Name, Node]),
+					master_agent_loop(ComputingUnit, NewAgentsAlive)
+			end;
+		{ remove, Node } ->
+			% check if the node is in the list of nodes 
+			case lists:member(Node, AgentsAlive) of
+				true ->
+					Node ! stop,
+					NewAgentsAlive = lists:delete(Node, AgentsAlive),
+					inform_local_agents(Node, NewAgentsAlive),
+					{_, Name} = process_info(Node, registered_name),
+					unregister(Name),
+					io:format("Node '~p' (~p) removed~n", [Name, Node]),
+					master_agent_loop(ComputingUnit, NewAgentsAlive);
+				_ ->
+					io:format("Node ~p is not found!~n", [Node]),
+					master_agent_loop(ComputingUnit, AgentsAlive)
+			end;
+		{ list_names, Pid } ->
+			Pid ! get_names(AgentsAlive),
+			master_agent_loop(ComputingUnit, AgentsAlive);
+		{ list_pids, Pid } ->
+			Pid ! AgentsAlive,
+			master_agent_loop(ComputingUnit, AgentsAlive);
 		stop -> 
 			io:format("Stopping the cluster...~n", []),
 			io:format("-----------------------~n", []),
 			stop_nodes(AgentsAlive),
 			unregister(master_node),
 			unregister(master_comp_node),
-			io:format("Stopped ~p~n", [master_node]);
-		pop -> 
-			% now we just remove last added node - 
-			% but later maybe we will remove the node
-			% by some criteria - least loaded etc
-			Node = lists:last(AgentsAlive),
-			Node ! stop,
-			% inform_local_agents(Node),
-			Name = list_to_atom(lists:flatten(io_lib:format("node_~p", [node_id(AgentsAlive)]))),
-			unregister(Name)
-		% { remove, Pid } ->
+			io:format("Stopped ~p~n", [master_node])
 	end,
 	master_agent_loop(ComputingUnit, AgentsAlive).
+
+local_agent_loop(ComputingUnit, Master, AgentsAlive) -> 
+	receive
+		{ connect, NewPid } ->
+			local_agent_loop(ComputingUnit, Master, [NewPid | AgentsAlive]);
+		{ removed, Node } ->
+			io:format("'~p' notified about removal of (~p) ~n", [self(), Node]),
+			local_agent_loop(ComputingUnit, Master, lists:delete(Node, AgentsAlive));
+		stop -> 
+			{ _, Name } = process_info(ComputingUnit, registered_name),
+			unregister(Name),
+			ok
+	end,
+	local_agent_loop(ComputingUnit, Master, AgentsAlive).
+
+empty_computation_loop(AgentPid) ->
+	empty_computation_loop(AgentPid).
 
 % TODO: Later when the nodes can be remote - add ping and all this things
 connect_local_agents(_, []) -> ok;
@@ -88,49 +132,63 @@ connect_local_agents(NewPid, [ Agent | T ]) ->
 	io:format("conneced to  ~p~n", [Agent]),
 	connect_local_agents(NewPid, T).
 
-empty_computation_loop(AgentPid) ->
-	empty_computation_loop(AgentPid).
-
-local_agent_loop(ComputingUnit, Master, AgentsAlive) -> 
-	receive
-		{ connect, NewPid } ->
-			local_agent_loop(ComputingUnit, Master, [NewPid | AgentsAlive]);
-		stop -> 
-			{ _, Name } = process_info(ComputingUnit, registered_name),
-			unregister(Name),
-			ok
-	end,
-	local_agent_loop(ComputingUnit, Master, AgentsAlive).
-
-add_node(Pid) -> 
-	Pid ! add.
-
 stop_nodes([]) -> ok;
 stop_nodes([Agent]) ->
-	Agent ! stop,
 	{ _, Name } = process_info(Agent, registered_name),
+	Agent ! stop,
 	unregister(Name),
 	io:format("Stopped ~w~n", [Name]);
 stop_nodes([Agent | T]) ->
-	Agent ! stop,
 	{ _, Name } = process_info(Agent, registered_name),
+	Agent ! stop,
 	unregister(Name),
 	io:format("Stopped ~w~n", [Name]),
 	stop_nodes(T).
 
+inform_local_agents(_, []) -> ok;
+inform_local_agents(Node, [Agent]) ->
+	Agent ! { removed, Node};
+inform_local_agents(Node, [Agent | T]) ->
+	Agent ! { removed, Node},
+	inform_local_agents(Node, T).
+
+get_names([]) -> [];
+get_names([ Pid ]) ->
+	{ _, Name } = process_info(Pid, registered_name),
+	[Name];
+get_names([Pid | T]) ->
+	{ _, Name } = process_info(Pid, registered_name),
+	[Name] ++ get_names(T).
+
+
+% util for generating the tokens to register the processes
+next_node_id([]) -> 1;
+next_node_id([_]) -> 2;
+next_node_id([_ | T]) -> 1 + next_node_id(T).
+
+
+% Interface functions
 stop_cluster(Pid) ->
 	Pid ! stop.
 
-% util for generating the tokens to register the processes
-node_id([]) -> 1;
-node_id([_]) -> 2;
-node_id([_ | T]) -> 1 + node_id(T).
+add_node(Pid) -> 
+	Pid ! add.
 
 pop_node(Master) ->
-	Master ! { pop }.
+	Master ! pop.
+
+get_node_names(Master) -> 
+	Master ! {list_names, self()},
+	receive
+		L -> L
+	end.
+
+get_node_pids(Master) ->
+	Master ! {list_pids, self()},
+	receive
+		L -> L
+	end.
 
 remove_node(Master, Pid) ->
 	Master ! { remove, Pid }.
 
-% inform_local_agents() ->.
-% stop_computing_unit()
